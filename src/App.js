5b15287b5de81f425c7eff108a214d9107a289ad
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-05-25 12:50:23 -0300

    :sparkles: feat: data fetching

--- src/App.js
+++ src/App.js
@@ -10,22 +10,36 @@ class App extends Component {
     this.loadPosts();
   }
 
-  loadPosts() {
-    const resp = fetch('https://jsonplaceholder.typicode.com/posts');
-    resp.then(r=>r.json()).then(posts => this.setState({ posts }));
+  loadPosts = async () => {
+    const postsResp = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResp = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [postsResult, photosResult] = await Promise.all([postsResp, photosResp]);
+
+    let posts = await postsResult.json();
+    const photos = await photosResult.json();
+
+    posts = posts.map((p, i) => ({ ...p, url: photos[i].url }))
+
+    this.setState({ posts })
   }
 
   render() {
     const { posts } = this.state;
     return (
-      <div className='posts'>
-        {posts.map(p =>
-          <div key={p.id} className='post-card'>
-            <h1>{p.title}</h1>
-            <p>{p.body}</p>
-          </div>
-        )}
-      </div>
+      <section className='container'>
+        <div className='posts'>
+          {posts.map(p =>
+            <div key={p.id} className='post-card'>
+              <img src={p.url} alt={p.title} />
+              <div className='post-content'>
+                <h4>{p.title}</h4>
+                <p>{p.body}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
     );
   }
 }
