d2a323ecd9fcfe335f5d703476b6d65d5a51e018
shimigui <miguel.nasto@gmail.com>
2024-06-19 18:36:06 -0300

    feat: formating <PostCard /> render on <PostGrid/> element

--- src/components/PostGrid/index.jsx
+++ src/components/PostGrid/index.jsx
@@ -3,13 +3,6 @@ import { PostCard } from "../PostCard";
 
 export const PostGrid = ({ posts = [] }) => (
     <div className="post-grid">
-        {!!posts &&
-            posts.map((p, i) => <PostCard
-                key={p.id ?? i}
-                title={p.title}
-                body={p.body}
-                url={p.url}
-            />)
-        }
+        {!!posts && posts.map(p => <PostCard {...p} key={p.id} />)}
     </div>
 )
